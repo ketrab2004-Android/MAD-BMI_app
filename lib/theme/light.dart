9c5b1ac4c45119fc095851f226d1db8ba95a113e
Bartek O <bartek@fos.nl>
2021-11-29 21:43:21 +0100

    Add appBarTheme to lightTheme

--- lib/theme/light.dart
+++ lib/theme/light.dart
@@ -2,11 +2,18 @@ part of themes;
 
 class LightThemeData
 {
+  static final Brightness brt = Brightness.light;
+
   static final ThemeData theme = ThemeData(
-    brightness: Brightness.light,
+    brightness: brt,
 
     primaryColor: Colors.lightBlueAccent,
     accentColor: Colors.white,
+
+    appBarTheme: AppBarTheme(
+      brightness: brt,
+      color: Colors.lightBlueAccent,
+    ),
 
     fontFamily: 'Comic Sans',
   );
