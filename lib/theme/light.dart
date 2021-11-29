cb87dfe2cdc6499e6bcf13fafbc6165d021d2486
Bartek O <bartek@fos.nl>
2021-11-29 21:27:11 +0100

    Added lightTheme to themes library

---
+++ lib/theme/light.dart
@@ -0,0 +1,13 @@
+part of themes;
+
+class LightThemeData
+{
+  static final ThemeData theme = ThemeData(
+    brightness: Brightness.light,
+
+    primaryColor: Colors.lightBlueAccent,
+    accentColor: Colors.white,
+
+    fontFamily: 'Comic Sans',
+  );
+}
