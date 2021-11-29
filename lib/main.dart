910e92188cc8626e0f5b8ba4d5f9f2694e726aef
Bartek O <bartek@fos.nl>
2021-11-29 21:47:27 +0100

    Made main use new WidgetLibrary

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:bmi_calculator/body/InputPage.dart';
-
+import 'package:bmi_calculator/widget/widgetLibrary.dart' as Widgets;
 import 'package:bmi_calculator/theme/themeLibrary.dart' as AppThemes;
 
 void main() => runApp(BMICalculator());
@@ -10,7 +9,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
+      home: Widgets.InputPage(),
 
       theme: AppThemes.LightThemeData.theme,
       darkTheme: AppThemes.DarkThemeData.theme,
