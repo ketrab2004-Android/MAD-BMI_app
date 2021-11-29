155852ba51a7223ab6bd471664cb6a83b818c482
Bartek O <bartek@fos.nl>
2021-11-29 21:27:20 +0100

    Added darkTheme to themes library

---
+++ lib/theme/dark.dart
@@ -0,0 +1,13 @@
+part of themes;
+
+class DarkThemeData extends LightThemeData
+{
+  static final ThemeData theme = ThemeData(
+    brightness: Brightness.dark,
+
+    primaryColor: Colors.blue,
+    accentColor: Colors.black54,
+
+    fontFamily: 'Comic Sans',
+  );
+}
