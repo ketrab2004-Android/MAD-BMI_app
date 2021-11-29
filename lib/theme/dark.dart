ab8d8a3e4efcf8f6dffcbfdae7edbbe28f842a16
Bartek O <bartek@fos.nl>
2021-11-29 21:43:31 +0100

    Add appBarTheme to darkTheme

--- lib/theme/dark.dart
+++ lib/theme/dark.dart
@@ -2,11 +2,18 @@ part of themes;
 
 class DarkThemeData extends LightThemeData
 {
-  static final ThemeData theme = ThemeData(
-    brightness: Brightness.dark,
+  static final Brightness brt = Brightness.dark;
 
-    primaryColor: Colors.blue,
-    accentColor: Colors.black54,
+  static final ThemeData theme = ThemeData(
+    brightness: brt,
+
+    primaryColor: Colors.indigo,
+    accentColor: Colors.black87,
+
+    appBarTheme: AppBarTheme(
+      brightness: brt,
+      color: Colors.indigo,
+    ),
 
     fontFamily: 'Comic Sans',
   );
