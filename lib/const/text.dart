d74e83d1dcaeef6eda32edbaf6fe4275f85d28db
Bartek O <bartek@fos.nl>
2021-12-06 20:49:45 +0100

    Created class for string constants

---
+++ lib/const/text.dart
@@ -0,0 +1,24 @@
+part of consts;
+
+class Texts
+{
+  static Texts createFromLanguage()
+  {
+    return Texts(); // temporary default
+  }
+
+  Locale locale = Locale('en', 'GB');
+
+  String appTitle = "BMI CALCULATOR";
+
+  String genderMale = "Male";
+  String genderFemale = "Female";
+
+  String height = "Height";
+  String heightMeasurement = "cm";
+
+  String weight = "Weight";
+  String age = "Age";
+
+  String calculateButton = "Calculate your BMI";
+}
