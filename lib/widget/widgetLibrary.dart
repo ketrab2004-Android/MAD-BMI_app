2baf2878526e3a150367a56629a51365ce5b2730
Bartek O <bartek@fos.nl>
2021-12-14 11:52:21 +0100

    Added new widgets to widget library

--- lib/widget/widgetLibrary.dart
+++ lib/widget/widgetLibrary.dart
@@ -4,3 +4,5 @@ import 'package:flutter/material.dart';
 import 'package:bmi_calculator/theme/themeLibrary.dart';
 
 part 'StyledContainer.dart';
+part 'InkyContainer.dart';
+part 'SquareButton.dart';
