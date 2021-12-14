904896f2136ae2d05979dea6c7d568d781703117
Bartek O <bartek@fos.nl>
2021-12-14 10:40:08 +0100

    Created "custom" widget with styling

--- lib/widget/StyledContainer.dart
+++ lib/widget/StyledContainer.dart
@@ -1,18 +1,30 @@
 part of widgets;
 
-class StyledContainer
-{
-  static Container get({
-    Widget child,
-    EdgeInsetsGeometry padding = const EdgeInsets.all(8.0),
-    AlignmentGeometry alignment = Alignment.topLeft
-  }) {
+class StyledContainer extends StatelessWidget {
+  StyledContainer({
+    @required this.theme,
+    this.child,
+    this.colour, // default is from this.theme.scheme
+    this.padding = const EdgeInsets.all(8.0),
+    this.alignment = Alignment.topLeft,
+  });
+
+  final ThemeHolder theme;
+  final Color colour;
+  final Widget child;
+  final EdgeInsetsGeometry padding;
+  final AlignmentGeometry alignment;
+
+  @override
+  Widget build(BuildContext context) {
     return new Container(
       child: child,
       padding: padding,
       alignment: alignment,
-
-
+      decoration: BoxDecoration(
+        borderRadius: theme.borderRadius,
+        color: colour ?? theme.scheme.secondary,
+      ),
     );
   }
 }
